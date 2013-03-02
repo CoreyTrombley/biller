4f5dc086501dfe5afa9c78e39389da9356ec4df3
Corey Trombley <corey.trombley33@gmail.com>
2013-03-02 09:53:07 -0500

    added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+User.delete_all
+Bill.delete_all
+
+u1 = User.create(:email => "bob@test.com", :password => 'a', :password_confirmation => 'a')
+
+b1 = Bill.create(:name => 'Rent', :description => 'the rent', :amount => 10_000, :day => 1)
+b2 = Bill.create(:name => 'Cable', :description => 'Cable bill', :amount => 150, :day => 5)
+b3 = Bill.create(:name => 'Credit Card', :description => 'credit card one', :amount => 55, :day => 11)
+
+u1.bills = [b1, b2, b3]
