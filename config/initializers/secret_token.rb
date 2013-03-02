b9a1d682c10817c9e9ee3fccc00bdf1a40326735
Corey Trombley <corey.trombley33@gmail.com>
2013-03-02 09:24:28 -0500

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Biller::Application.config.secret_token = '8f906d33d36739877b7e244d4801da7a1cdec1756eec8c33ec45367854d7883e7115a2fd0d2cf15e06006a8fb039184fc8c3ec8f9ba80736feb204029d7bfba0'
