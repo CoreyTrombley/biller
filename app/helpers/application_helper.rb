8a71f17427e7db3a78409bd6c76086dc6e942ef3
Corey Trombley <corey.trombley33@gmail.com>
2013-03-02 14:16:44 -0500

    added views, controllers and models

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+  def intellinav
+    if @auth.present?
+      link_to(@auth.email.capitalize, login_path, :method => :delete, :confirm => 'Realy, you want to log off?')
+    else
+      link_to('Login', login_path)
+    end
+  end
 end
